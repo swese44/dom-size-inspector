76b1e3127fa19d90b5b6fac2b3efca183e360718
P.J. Swesey <pjsw@microsoft.com>
2020-08-06 12:45:17 -0700

    Converting to TypeScript and setting up the build

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # dom-size-inspector
 
-A Chrome browser extension to highlight heavy DOM nodes and show opportunities for reducing elements. Also shows total number of DOM nodes and max depth.
+A Chrome browser extension to highlight heavy DOM nodes and show opportunities for reducing elements. Also shows total number of DOM nodes and max depth. It can be installed here:
+
+https://chrome.google.com/webstore/detail/dom-size-inspector/akaionidnagpoippeififmdblfihmjnb
--- src/background.ts
+++ src/background.ts
@@ -1,10 +1,8 @@
 chrome.runtime.onConnect.addListener(function (devToolsConnection) {
-  // assign the listener function to a variable so we can remove it later
-  var devToolsListener = function (message, sender, sendResponse) {
-    // Inject a content script into the identified tab
+  const devToolsListener = (message) => {
     chrome.tabs.executeScript(message.tabId, { file: message.scriptToInject });
   };
-  // add the listener
+
   devToolsConnection.onMessage.addListener(devToolsListener);
 
   devToolsConnection.onDisconnect.addListener(function () {
